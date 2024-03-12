5f7338fa77f2704de8e40c0aac722788035c2803
pratik <pratikgmre13@gmail.com>
2024-03-12 12:43:14 +0545

    add intial files

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const EmployeeModel = require("./models/Employee.js");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb+srv://Pkg:pratik123@cluster2.gcrihih.mongodb.net/employee")
+  .then(() => {
+    console.log("mongodb connected");
+  });
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  EmployeeModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("success");
+      } else {
+        res.json("the password is incorrect");
+      }
+    } else {
+      res.json("not email register");
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  EmployeeModel.create(req.body)
+    .then((employees) => res.json(employees))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("server is running");
+});
